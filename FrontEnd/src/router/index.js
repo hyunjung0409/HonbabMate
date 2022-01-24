5343759444c8472562d1ace03b8c68f5a033923b
이현정 <h.jung0409@daum.net>
2022-01-24 16:55:26 +0900

    FEAT feature_채팅 옵션 페이지 추가

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,49 +1,74 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Manual from "../views/Manual.vue";
-import FAQ from "../views/FAQ.vue";
-import About from "../views/About.vue";
-import Loading from "../views/Loading.vue";
-import Chatroom from "../views/Chatroom.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// import Home from '../views/Home.vue';
+import Manual from '../views/Manual.vue';
+import FAQ from '../views/FAQ.vue';
+import About from '../views/About.vue';
+import Loading from '../views/Loading.vue';
+import Chatroom from '../views/Chatroom.vue';
+
+import ChatOption from '@/views/ChatOption.vue';
+import SilenceOrTalk from '@/components/chat_option/SilenceOrTalk.vue';
+import NumberOfPeople from '@/components/chat_option/NumberOfPeople.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home,
+  // },
   {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     component: About,
   },
   {
-    path: "/manual",
-    name: "Manual",
+    path: '/manual',
+    name: 'Manual',
     component: Manual,
   },
   {
-    path: "/FAQ",
-    name: "FAQ",
+    path: '/FAQ',
+    name: 'FAQ',
     component: FAQ,
   },
   {
-    path: "/loading",
-    name: "Loading",
+    path: '/loading',
+    name: 'Loading',
     component: Loading,
   },
 
   {
-    path: "/chatroom",
-    name: "Chatroom",
+    path: '/chatroom',
+    name: 'Chatroom',
     component: Chatroom,
+  },
+  {
+    // path: '/chat-option',
+    path: '/',
+    name: 'ChatOption',
+    component: ChatOption,
+    redirect: '/people',
+    children: [
+      {
+        path: 'people',
+        name: 'NumberOfPeople',
+        component: NumberOfPeople,
+      },
+      {
+        path: 'talk',
+        name: 'SilenceOrTalk',
+        component: SilenceOrTalk,
+      },
+    ],
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
