c58fce7896a352709599fe8fccdc9e3953c2a4ad
이현정 <h.jung0409@daum.net>
2022-02-03 23:53:46 +0900

    FEAT 채팅룸 더보기 버튼 추가

---
+++ FrontEnd/src/components/chat_room/DropdownMenu.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="text-center">
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="200"
+      left
+      offset-y
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon color="white" dark v-bind="attrs" v-on="on">
+          <v-icon> mdi-dots-horizontal-circle-outline </v-icon>
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar>
+              <img
+                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                alt="profile-picture"
+              />
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>혼밥메이트206</v-list-item-title>
+              <v-list-item-subtitle>
+                좋아요 : {{ countLike }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn :class="fav ? 'red--text' : ''" icon @click="fav = !fav">
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+
+        <v-divider />
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>최애음식</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <!-- <v-list-item-action>
+              <v-switch
+                v-model="hints"
+                color="purple"
+              />
+            </v-list-item-action> -->
+            <v-list-item-title>또 뭐있지?</v-list-item-title>
+          </v-list-item>
+        </v-list>
+
+        <v-divider />
+
+        <v-dialog v-model="blockDialog" width="500">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn text v-bind="attrs" v-on="on"> 차단 </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="text-h5 grey lighten-2">
+              혼밥메이트206 님을 차단하시겠습니까?
+            </v-card-title>
+
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="error" text @click="blockDialog = false">
+                차단
+              </v-btn>
+              <v-btn color="primary" text @click="blockDialog = false">
+                취소
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="reportDialog" width="500">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn text v-bind="attrs" v-on="on"> 신고 </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="text-h5 grey lighten-2">
+              혼밥메이트206 님을 신고하시겠습니까?
+            </v-card-title>
+
+            <v-card-text>
+              <v-textarea
+                class="mt-5"
+                label="신고 사유"
+                auto-grow
+                outlined
+                rows="4"
+                row-height="30"
+              />
+            </v-card-text>
+
+            <v-divider />
+
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="error" text @click="reportDialog = false">
+                신고
+              </v-btn>
+              <v-btn color="primary" text @click="reportDialog = false">
+                취소
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <v-card-actions>
+          <v-spacer />
+
+          <v-btn text @click="menu = false"> 닫기 </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DropdownMenu",
+  data: () => ({
+    fav: true,
+    menu: false,
+    blockDialog: false,
+    reportDialog: false,
+  }),
+};
+</script>
+
+<style></style>
--- FrontEnd/src/components/chat_room/UserVideo.vue
+++ FrontEnd/src/components/chat_room/UserVideo.vue
@@ -1,25 +1,37 @@
 <template>
   <div v-if="streamManager" align="center">
-    <ov-video :stream-manager="streamManager" />
-    <div class="mt-3" align="center">
-      <p>{{ clientData }}</p>
-    </div>
+    <a style="cursor: default">
+      <div class="video-area">
+        <div class="top">
+          <dropdown-menu />
+        </div>
+
+        <ov-video :stream-manager="streamManager" />
+      </div>
+    </a>
   </div>
 </template>
 
 <script>
 import OvVideo from "@/components/chat_room/OvVideo";
+import DropdownMenu from "@/components/chat_room/DropdownMenu";
 
 export default {
   name: "UserVideo",
 
   components: {
     OvVideo,
+    DropdownMenu,
   },
 
   props: {
     streamManager: Object,
   },
+
+  data: () => ({
+    statusLike: false,
+    statusMore: false,
+  }),
 
   computed: {
     clientData() {
@@ -33,6 +45,26 @@ export default {
       const { connection } = this.streamManager.stream;
       return JSON.parse(connection.data);
     },
+    toggleLike() {
+      this.statusLike = !this.statusLike;
+    },
+    toggleMore() {
+      this.statusMore = !this.statusMore;
+    },
   },
 };
 </script>
+
+<style>
+a .video-area {
+  position: relative;
+  /* overflow: hidden; */
+}
+
+a .video-area .top {
+  position: absolute;
+  top: 2%;
+  left: 90%;
+  z-index: 2;
+}
+</style>
