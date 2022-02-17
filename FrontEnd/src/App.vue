6361ef73dcd500e6cf0120e66d857ad02107b355
이현정 <h.jung0409@daum.net>
2022-02-17 14:22:44 +0900

    FEAT_Footer 추가

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -12,14 +12,18 @@
     <v-main style="background-color: #eeeeee">
       <router-view></router-view>
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Footer from "@/components/layout/Footer";
+
 export default {
   name: "App",
   components: {
     Navbar: () => import("@/components/layout/Navbar"),
+    Footer,
   },
 
   data: () => ({
